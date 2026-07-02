db0c48c6a61a467d7daba5a37f27a6629ac7a657
Utkarsh Dhande <utkarshdhande1432@gmail.com>
2026-07-02 22:55:44 +0530

    Defer

---
+++ 01_Basics/10_Defer/main.go
@@ -0,0 +1,31 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	defer fmt.Println("world")
+
+// 	fmt.Println("hello")
+// }
+
+// NOTE: A defer statement defers the execution of a function until the surrounding function returns. The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+
+/////////////////////////////////////////////////////
+
+// Stacking defers
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
+
+// NOTE: Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order. 
