59c7cf416494e3af4b319294e9e9ee79d40bbda6
Utkarsh Dhande <utkarshdhande1432@gmail.com>
2026-07-22 16:41:52 +0530

    default selection

--- 04_Concurrency/main.go
+++ 04_Concurrency/main.go
@@ -104,33 +104,76 @@
 
 // Select
 
+// package main
+
+// import "fmt"
+
+// func fibonacci(c, quit chan int) {
+// 	x, y := 0, 1
+// 	for {
+// 		select {
+// 		case c <- x:
+// 			x, y = y, x+y
+// 		case <-quit:
+// 			fmt.Println("quit")
+// 			return
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	c := make(chan int)
+// 	quit := make(chan int)
+// 	go func() {
+// 		for i := 0; i < 10; i++ {
+// 			fmt.Println(<-c)
+// 		}
+// 		quit <- 0
+// 	}()
+// 	fibonacci(c, quit)
+// }
+
+// NOTE: The `select` statement lets a goroutine wait on multiple communication operations. A `select` blocks until one of its cases can run, then it executes that case. If multiple cases can proceed, then one case is chosen at random to execute. The `select` statement is like a `switch`, but for channels. In this example, the `fibonacci` function sends Fibonacci numbers to channel `c` until it receives a signal on the `quit` channel, at which point it prints "quit" and returns.
+
+/////////////////////////////////////////////////////////////////////
+
+// Default Selection
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func fibonacci(c, quit chan int) {
-	x, y := 0, 1
+func main() {
+	start := time.Now()
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	elapsed := func() time.Duration {
+		return time.Since(start).Round(time.Millisecond)
+	}
 	for {
 		select {
-		case c <- x:
-			x, y = y, x+y
-		case <-quit:
-			fmt.Println("quit")
+		case <-tick:
+			fmt.Printf("[%6s] tick.\n", elapsed())
+		case <-boom:
+			fmt.Printf("[%6s] BOOM!\n", elapsed())
 			return
+		default:
+			fmt.Printf("[%6s]     .\n", elapsed())
+			time.Sleep(50 * time.Millisecond)
 		}
 	}
 }
 
-func main() {
-	c := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(<-c)
-		}
-		quit <- 0
-	}()
-	fibonacci(c, quit)
-}
+// NOTE: The default case in a select is run if no other case is ready.
 
-// NOTE: The `select` statement lets a goroutine wait on multiple communication operations. A `select` blocks until one of its cases can run, then it executes that case. If multiple cases can proceed, then one case is chosen at random to execute. The `select` statement is like a `switch`, but for channels. In this example, the `fibonacci` function sends Fibonacci numbers to channel `c` until it receives a signal on the `quit` channel, at which point it prints "quit" and returns.
+// Use a default case to try a send or receive without blocking:
+
+// select {
+// case i := <-c:
+//     // use i
+// default:
+//     // receiving from c would block
+// }
